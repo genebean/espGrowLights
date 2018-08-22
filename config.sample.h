91c5af7422c2919b5f90ab7f04cb46e86c7e5420
Gene Liverman <gene+06108928@technicalissues.us>
2018-08-21 20:56:59 -0400

    Renamed and expanded included file

---
+++ config.sample.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "...."
+#define WIFI_PASS "...."
+
+// The name that will show up in Amazon
+// Note: Alexa thought I was saying "Garage Light" when I said "Grow Light" so I changed it :)
+#define DEVICE_NAME "Disco Lights"
+
+// The DNS name
+#define HOST_NAME "espGrowLights"
